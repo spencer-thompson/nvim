a3b76366993d03fe6bede6bf0f8983f0342c7d30
Spencer <sthompson.dev@outlook.com>
2024-01-08 23:38:13 -0700

    added some ai plugins, not tested not used yet

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,12 @@
+return {
+    { -- requires subscription
+        'github/copilot.vim',
+        event = 'VeryLazy',
+        lazy = true,
+    },
+    -- {
+    --     'David-Kunz/gen.nvim',
+    --     event = 'VeryLazy',
+    --     lazy = true,
+    -- }
+}
