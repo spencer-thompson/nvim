eae88fe6c086a7a2cc6ebdf7fa62684ae1a431e7
Spencer <sthompson.dev@outlook.com>
2024-01-09 23:12:18 -0700

    quick change to noice

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -105,7 +105,7 @@ return {
                         height = "auto",
                     },
                     border = {
-                        style = "single",
+                        style = "none",
                         padding = { 2, 3 },
                     },
                     win_options = {
