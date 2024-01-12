8b3a70dfe53d98c42108a50402441c29382d1695
Spencer <sthompson.dev@outlook.com>
2024-01-12 00:00:50 -0700

    trying to add via arch but git won't let me

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -9,5 +9,14 @@ return {
         event = 'VeryLazy',
         lazy = true,
 
+        config = function()
+            require('gen').setup({})
+
+            require('gen').prompts['LaTeX'] = {
+                prompt =
+                "Modify the following input to format it as proper LaTeX, just output the final LaTeX without additional quotes around it:\n$text",
+                replace = true,
+            }
+        end
     }
 }
