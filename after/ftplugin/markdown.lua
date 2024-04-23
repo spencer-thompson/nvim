35faa1eb26972f41c889e9bf8baf8fbc12536654
Spencer <sthompson.dev@outlook.com>
2024-04-23 03:13:48 -0600

    wrote cool pdf previewr for markdown pandoc

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,9 +4,37 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.wrap = true
 vim.opt_local.ff = 'unix'
 
--- local keymaps
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>r', '<cmd>MarkdownPreview<CR>',
-    { desc = 'Markdown Preview' })
+vim.b.markdown_pdf_preview = false
 
-vim.api.nvim_buf_set_keymap(0, 'n', '<leader>p', '<cmd>execute "!pandoc -i % -o " expand("%:r") .. ".pdf"<CR>',
-    { desc = 'Pandoc -> PDF' })
+-- auto commands
+local group = vim.api.nvim_create_augroup('Markdown PDF Preview', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+    desc = 'Live update compiled PDF',
+    pattern = '*.md',
+    group = group,
+    callback = function()
+        if vim.b.markdown_pdf_preview then
+            vim.cmd([[silent !pandoc -i % -o "%:r".pdf]])
+        end
+    end,
+})
+
+-- local keymaps
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>r', '<cmd>MarkdownPreview<CR>', { desc = 'Markdown Preview' })
+vim.keymap.set('n', '<leader>R', function()
+    if vim.b.markdown_pdf_preview then
+        vim.b.markdown_pdf_preview = false
+    else
+        vim.b.markdown_pdf_preview = true
+        vim.cmd([[silent !pandoc -i % -o "%:r".pdf]])
+        vim.cmd([[silent !zathura "%:r".pdf &]])
+    end
+end, { desc = 'Markdown PDF Preview' })
+
+vim.api.nvim_buf_set_keymap(
+    0,
+    'n',
+    '<leader>p',
+    '<cmd>execute "!pandoc -i % -o " expand("%:r") .. ".pdf"<CR>',
+    { desc = 'Pandoc -> PDF' }
+)
