a2d9f8b7e40ebaf6ff76d1d6ee3ab06cbc5136a9
Spencer <sthompson.dev@outlook.com>
2024-07-18 22:41:56 -0600

    added nvim spectre

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -59,4 +59,22 @@ return {
         event = 'VeryLazy',
         opts = {}, --config
     },
+
+    {
+        'nvim-pack/nvim-spectre',
+        name = 'spectre',
+        event = 'VeryLazy',
+        build = false,
+        cmd = 'Spectre',
+        opts = { open_cmd = 'noswapfile vnew' },
+        keys = {
+            {
+                '<leader>sr',
+                function()
+                    require('spectre').open()
+                end,
+                desc = 'Replace in Files (Spectre)',
+            },
+        },
+    },
 }
