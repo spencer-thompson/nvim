12e5509807161e168cd4d724784893dc97161533
Spencer <sthompson.dev@outlook.com>
2024-10-27 16:36:56 -0600

    fixes python cwd issues and cleaned up typst previewing

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -31,7 +31,7 @@ vim.api.nvim_buf_set_keymap(
     0,
     'n',
     '<leader>r',
-    '<cmd>TermExec cmd="python %" dir=%<CR>',
+    '<cmd>TermExec cmd="python %:t" dir=%:p:h<CR>',
     { desc = '[R]un Current File' }
 )
 
--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -2,7 +2,6 @@
 -- vim.opt_local.shiftwidth = 2
 vim.opt_local.wrap = true
 vim.bo.commentstring = '// %s'
--- vim.bo.comments = 'comments=s1:/*,mb:*,ex:*/,://' -- this breaks stuff
 
 vim.api.nvim_buf_set_var(0, 'active_preview', false)
 
@@ -41,28 +40,6 @@ local compile_doc = Job:new({
         end
     end,
 })
-
--- auto commands
-local group = vim.api.nvim_create_augroup('Markdown PDF Preview', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-    desc = 'Live update compiled PDF',
-    pattern = '*.md',
-    group = group,
-    callback = function()
-        if vim.api.nvim_buf_get_var(0, 'active_preview') then
-            compile_doc:start()
-        end
-    end,
-})
-
--- local keymaps
--- vim.api.nvim_buf_set_keymap(
---     0,
---     'n',
---     '<leader>R',
---     '<cmd>MarkdownPreviewToggle<CR>',
---     { desc = 'Markdown Browser Preview' }
--- )
 
 vim.keymap.set('n', '<leader>r', function()
     if vim.api.nvim_buf_get_var(0, 'active_preview') then
