3ee1cde35f86ec32fe207e3163fe5b02a77faebe
Spencer <sthompson.dev@outlook.com>
2025-05-26 17:04:13 -0600

    plugin bump and messing with minimap

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -427,7 +427,7 @@ return {
 
     { -- one of my favorite plugins, this shows the minimap on the side
         'Isrothy/neominimap.nvim',
-        enabled = false,
+        enabled = true,
         name = 'neominimap',
         event = 'VeryLazy',
         lazy = false,
