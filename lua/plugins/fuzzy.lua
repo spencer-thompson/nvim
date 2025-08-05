d43e23c8199ad043f9af507315e0c0df38762c7f
Spencer <sthompson.dev@outlook.com>
2025-08-05 00:20:14 -0600

    tinkering with fff

--- lua/plugins/fuzzy.lua
+++ lua/plugins/fuzzy.lua
@@ -2,14 +2,25 @@ return {
     {
         'dmtrKovalenko/fff.nvim',
         build = 'cargo build --release',
+        lazy = true,
         -- or if you are using nixos
         -- build = "nix run .#release",
         opts = {
             -- pass here all the options
+            title = 'Find Files',
+            prompt = '||> ',
+            width = 0.85,
+            height = 0.85,
+            preview = {
+                width = 0.6,
+            },
             keymaps = {
                 select = { '<CR>', '<C-y>' },
                 move_up = { '<Up>', '<C-k>' },
                 move_down = { '<Down>', '<C-j>' },
+            },
+            hl = {
+                prompt = 'Constant',
             },
         },
         keys = {
