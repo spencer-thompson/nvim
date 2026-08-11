294a0a09bdb86e0098e87ad77ee17ad88f1249ff
Spencer <sthompson.dev@outlook.com>
2026-08-11 18:53:35 -0400

    updated plugins and removed rumdl auto formatting on md for now

--- lua/pack/format.lua
+++ lua/pack/format.lua
@@ -30,7 +30,7 @@ vim.schedule(function()
             -- java = { "google-java-format" },
             -- kotlin = { "ktlint" },
             -- ruby = { "standardrb" },
-            markdown = { 'rumdl' },
+            -- markdown = { 'rumdl' },
             -- erb = { "htmlbeautifier" },
             html = { 'prettierd' },
             bash = { 'shfmt' },
